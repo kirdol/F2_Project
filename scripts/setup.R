3b110f96d5fe4640a3636d9612b29f796bd48688
kirdol <lodrik.adam@hotmail.ch>
2024-04-24 13:50:01 +0200

    MàJ setup

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,6 +1,7 @@
 # load the required packages and install them if they are not.
 packages_loaded <- c(
-  "here"
+  "here",
+  "DT"
 )
 
 # Function that install the packages if not already installed on your computer
